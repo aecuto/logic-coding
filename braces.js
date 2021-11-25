e1edcb7868244ccfa61755ece50bc05214b446c6
aecuto <kampee.expertly01@gmail.com>
2021-11-25 12:51:06 +0700

    braces login done

---
+++ braces.js
@@ -0,0 +1,69 @@
+var prompt = require("prompt");
+
+prompt.start();
+
+const braces = ["(", ")", "{", "}", "[", "]"];
+
+const checkBraces = (str) => {
+  let firstBraces = [];
+  for (let i = 0; i < str.length; i++) {
+    const currStr = str[i];
+    const indexStr = braces.indexOf(currStr);
+    if (indexStr % 2) {
+      return validateBraces(firstBraces, str);
+    }
+    firstBraces.push(currStr);
+  }
+};
+
+const validateBraces = (firstBraces, str) => {
+  if (!firstBraces.length) {
+    return {
+      status: false,
+    };
+  }
+
+  const lastIndex = firstBraces.length - 1;
+
+  const secondBraces = str.slice(firstBraces.length, firstBraces.length * 2);
+  const nextGroup = str.slice(firstBraces.length * 2, str.length);
+
+  let secondIndex = 0;
+  for (let index = lastIndex; index >= 0; index--) {
+    const currStr = firstBraces[index];
+
+    const indexStr = braces.indexOf(currStr);
+
+    if (braces[indexStr + 1] !== secondBraces[secondIndex]) {
+      return {
+        status: false,
+      };
+    }
+    secondIndex++;
+  }
+
+  return {
+    status: true,
+    nextGroup,
+  };
+};
+
+prompt.get(["str"], function (err, result) {
+  console.log("Command-line input received:");
+  console.log("str: " + result.str);
+
+  let str = result.str;
+
+  let status = true;
+  do {
+    const result = checkBraces(str);
+    status = result.status;
+    str = result.nextGroup;
+
+    if (!str && status) {
+      break;
+    }
+  } while (status);
+
+  console.log({ status });
+});
